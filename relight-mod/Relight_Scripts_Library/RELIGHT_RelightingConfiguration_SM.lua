6443356386231cd5d3ecde5e6600bcc92d35269b
Kett <mdopslaff@gmail.com>
2025-05-07 19:40:26 +0200

    Michonne EP3
    Michonne EP3

--- relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_SM.lua
+++ relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_SM.lua
@@ -1,6 +1,12 @@
 --||||||||||||||||||||||||||||||||||||||| INI GENERATION - RELIGHT SEASON M |||||||||||||||||||||||||||||||||||||||
 --||||||||||||||||||||||||||||||||||||||| INI GENERATION - RELIGHT SEASON M |||||||||||||||||||||||||||||||||||||||
 --||||||||||||||||||||||||||||||||||||||| INI GENERATION - RELIGHT SEASON M |||||||||||||||||||||||||||||||||||||||
+--|||||||||||||||||||||||||||||||||||||||||||||||||||||| TODO |||||||||||||||||||||||||||||||||||||||||||||||||||||
+--||||||||||||||||||||||||||||||||||||||||||||||||| OpeningCredits ||||||||||||||||||||||||||||||||||||||||||||||||
+--|||||||||||||||||||||||||||||||||||||||||||||||||| PreviouslyOn |||||||||||||||||||||||||||||||||||||||||||||||||
+--|||||||||||||||||||||||||||||||||||||||||||||||||| NextTimeOn |||||||||||||||||||||||||||||||||||||||||||||||||||
+--|||||||||||||||||||||||||||||||||||||||||||||||||||||| Menu |||||||||||||||||||||||||||||||||||||||||||||||||||||
+
 
 RELIGHT_GenerateNewINI_SeasonM = function(number_configurationVersion)
     local data = 
@@ -77,7 +83,7 @@ RELIGHT_GenerateNewINI_SeasonM = function(number_configurationVersion)
         {
             RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M101_adv_shoreLineCove.lua",
         },
-        [Level_M101_TextMichonne] =
+        [Level_M101_TestMichonne] =
         {
             RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M101_adv_michonneZombiePets.lua",
         },
@@ -114,12 +120,6 @@ RELIGHT_GenerateNewINI_SeasonM = function(number_configurationVersion)
         {
             RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M102_adv_openingCredits102.lua",
         },
-        --[[ -- One day, maybe.
-        Level_M102_PreviouslyOn =
-        {
-            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M102_adv_previouslyOn.lua",
-        },
-        ]]
         Level_M102_ShorelineApproachWoods =
         {
             RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M102_adv_shorelineApproachWoods.lua",
@@ -132,11 +132,50 @@ RELIGHT_GenerateNewINI_SeasonM = function(number_configurationVersion)
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
-        Level_M03_ =
+        Level_M103_Epilogue =
         {
-            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
-        },
-
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_shorelineDock.lua",
+        },        
+        Level_M103_JohnsHouseExteriorLawn103 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_johnsHouseExteriorLawn103.lua",
+        },        
+        Level_M103_JohnsHouseExteriorLawn_negotiate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_negotiate_adv_johnsHouseExteriorLawn103.lua",
+        },        
+        Level_M103_JohnsHouseExteriorLawn_retreat =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_retreat_adv_johnsHouseExteriorLawn103.lua",
+        },        
+        Level_M103_JohnsHouseGarage =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_johnsHouseGarage.lua",
+        },        
+        Level_M103_JohnsHouseInteriorMain103 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_johnsHouseInteriorMain103.lua",
+        },        
+        Level_M103_JohnsHouseInteriorMainFire103 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_johnsHouseInteriorMainFire103.lua",
+        },        
+        Level_M103_JohnsHouseInteriorUpstairs103 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_johnsHouseInteriorUpstairs103.lua",
+        },        
+        Level_M103_JohnsHouseInteriorUpstairs_daughters =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_daughters_adv_johnsHouseInteriorUpstairsFire103.lua",
+        },        
+        Level_M103_JohnsHouseInteriorUpstairs_saveKids =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_johnsHouseInteriorUpstairs_SaveKids.lua",
+        },        
+        Level_M103_PeteBoatPrologue =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_M103_adv_peteBoatPrologue.lua",
+        },        
     };
         
     return data;
